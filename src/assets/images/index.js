51beabe161b631c38e6eecf507b8afcf10957f30
supineevil <75155924+supineevil@users.noreply.github.com>
2023-08-22 23:12:22 +0530

    updated files

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,9 +2,6 @@ import bigShoe1 from './big-shoe1.png'
 import bigShoe2 from './big-shoe2.png'
 import bigShoe3 from './big-shoe3.png'
 
-import customer1 from './customer1.jpeg'
-import customer2 from './customer2.svg'
-
 import footerLogo from './footer-logo.svg'
 import headerLogo from './header-logo.svg'
 import jordanLogo from './jordanLogo.png'
@@ -36,8 +33,7 @@ export {
     bigShoe2,
     bigShoe3,
 
-    customer1,
-    customer2,
+  
 
     footerLogo,
     headerLogo,
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
-import { bigShoe1, bigShoe2, bigShoe3, customer1, customer2, shoe4, shoe5, shoe6, shoe7, thumbnailShoe2, thumbnailShoe3,bigshoethumbnail2,bigshoethumbnail3
+import { bigShoe1, bigShoe2, bigShoe3, shoe4, shoe5, shoe6, shoe7, thumbnailShoe2, thumbnailShoe3,bigshoethumbnail2,bigshoethumbnail3
 } from "../assets/images";
 
 export const navLinks = [
